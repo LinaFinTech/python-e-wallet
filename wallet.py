50d7226af380cd98d137e57734bbc703d48bd163
LinaFinTech <linasalahharoun@gmail.com>
2026-08-25 22:16:08 +0800

    feat: add account input validation

--- wallet.py
+++ wallet.py
@@ -1,7 +1,28 @@
 def create_account():
-    name = input("Please enter your name: ")
-    email = input("Please enter your email: ")
-    password = input("Please enter your password: ")
+    while True:
+        name = input("Please enter your name: ")
+
+        if name.strip() != "":
+            break
+        else:
+            print("Please enter a valid name")
+
+    while True:
+        email = input("Please enter your email: ")
+
+        if email.strip() != "" and "@" in email and "." in email and len(email) > 7:
+            break
+        else:
+            print("Please enter a valid email")
+
+    while True:
+        password = input("Please enter your password: ")
+
+        if len(password) >= 8 and any(char.isupper() for char in password) and any(char.islower() for char in password) and any(char.isdigit() for char in password):
+            break
+        else:
+            print("Password must contain at least 8 characters, one uppercase letter, one lowercase letter, and one digit.")
+
 
     account = {
         "name": name,
@@ -12,4 +33,3 @@ def create_account():
 
     print("Account created successfully")
     return account
-
