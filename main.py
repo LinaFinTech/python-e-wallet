ee91d3071bc58950178077851d6bf84c435580a5
LinaFinTech <linasalahharoun@gmail.com>
2026-08-25 19:09:23 +0800

    add initial e-wallet menu and initial repository layout

---
+++ main.py
@@ -0,0 +1,9 @@
+print("=============================")
+print("       Python E-Wallet       ")
+print("=============================")
+
+print("1. Create Account")
+print("2. Login")
+print("3. Exit")
+
+print("Please select an option (1-3): ")
