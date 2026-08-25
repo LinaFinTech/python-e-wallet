85b17edbc2e3948cf30dce810a15a12d7bafd9e5
LinaFinTech <linasalahharoun@gmail.com>
2026-08-25 22:55:10 +0800

    feat: add in-memory account storage

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from wallet import create_account
+accounts = []
 
 def main_menu():
-    print("=============================")
-    print("       Python E-Wallet       ")
-    print("=============================")
+    while True:
+        print("=============================")
+        print("       Python E-Wallet       ")
+        print("=============================")
 
-    print("1. Create Account")
-    print("2. Login")
-    print("3. Exit")
+        print("1. Create Account")
+        print("2. Login")
+        print("3. Exit")
 
-    choice = int(input("Please select an option (1-3): "))
-    if choice == 1:
-        create_account()
+        choice = input("Please select an option (1-3): ")
+        if choice == "1":
+            new_account = create_account()
+            accounts.append(new_account)
+        elif choice == "2":
+            print("Login coming soon")
+        elif choice == "3":
+            print("Thank you for using Python E-Wallet")
+            break
+        else:
+            print("Please select an option (1-3): ")
+
+
 
 main_menu()
 
